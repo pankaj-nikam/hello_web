e22fd27f98d6c36c0b47b6504dd8aa99886e05de
Pankaj Nikam <pankaj-nikam@users.noreply.github.com>
2022-07-08 17:15:47 +0530

    Added divide feature. Added error handling.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"strconv"
@@ -44,14 +45,44 @@ func sum(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func divide(w http.ResponseWriter, r *http.Request) {
+	x, err := strconv.ParseFloat(r.URL.Query().Get("x"), 64)
+	if err != nil {
+		fmt.Fprintf(w, "Error occurred while converting x")
+	}
+	y, err := strconv.ParseFloat(r.URL.Query().Get("y"), 64)
+	if err != nil {
+		fmt.Fprintf(w, "Error occurred while converting y")
+	}
+	sum, err := DivideValues(x, y)
+	if err != nil {
+		fmt.Fprintf(w, "Error occurred: %s", err.Error())
+		return
+	}
+	n, err := fmt.Fprintf(w, "The sum of %f and %f is %f", x, y, sum)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Bytes written:", n)
+	}
+}
+
 func AddValues(x, y int) int {
 	return x + y
+}
+
+func DivideValues(x, y float64) (float64, error) {
+	if y == 0 {
+		return 0, errors.New("cannot divide by zero")
+	}
+	return x / y, nil
 }
 
 func main() {
 	http.HandleFunc("/", home)
 	http.HandleFunc("/about", about)
 	http.HandleFunc("/sum", sum)
+	http.HandleFunc("/divide", divide)
 	fmt.Println("Listening on port", portNumber)
 	_ = http.ListenAndServe(portNumber, nil)
 }
