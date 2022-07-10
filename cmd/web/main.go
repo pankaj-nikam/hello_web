4ede4c2afea5d1d95f1e6ca3556d8f3fe53f0d84
Pankaj Nikam <pankaj-nikam@users.noreply.github.com>
2022-07-10 19:10:09 +0530

    Added session middleware. Used session middleware.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/alexedwards/scs/v2"
 	"github.com/pankaj-nikam/hello_web/pkg/config"
 	"github.com/pankaj-nikam/hello_web/pkg/handlers"
 	"github.com/pankaj-nikam/hello_web/pkg/render"
@@ -12,8 +14,23 @@ import (
 
 const portNumber = ":3000"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 func main() {
-	app := config.AppConfig{}
+
+	//Change this to true when in production
+	app.IsProduction = false
+
+	session = scs.New()
+	session.Lifetime = time.Hour * 24
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.IsProduction
+	session.Cookie.HttpOnly = true
+
+	app.Session = session
+
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal(err)
